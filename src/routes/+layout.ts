e42ed9cc4b24e73692687f31fdb6049536d2bcdc
Omid Nikrah <omidnikrah@gmail.com>
2025-02-18 17:09:46 +0330

    fix: fix redirect url to payto protocol

---
+++ src/routes/+layout.ts
@@ -0,0 +1,18 @@
+import { redirect } from '@sveltejs/kit';
+
+export async function load({ url }) {
+	const { hostname, pathname, search, protocol } = new URL(url.href);
+
+	if (hostname === 'localhost' || hostname === 'payto.money') {
+		// const hasValidProtocol = protocol === 'payto.money:';
+		const cleanPath = pathname.startsWith('/://') ? pathname.slice(3) : pathname;
+		const fullPath = cleanPath.startsWith('/') ? cleanPath.slice(1) : cleanPath;
+		const redirectUrl = `payto://${fullPath}${search}`;
+
+		if (fullPath) {
+			throw redirect(302, redirectUrl);
+		}
+	}
+
+	return {};
+}
