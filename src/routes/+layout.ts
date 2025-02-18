1ffb1a7b3a38a834a0bfd3e105de81626ec9b739
Omid Nikrah <omidnikrah@gmail.com>
2025-02-18 17:17:56 +0330

    fix: fix redirect url to show page

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -5,9 +5,9 @@ export async function load({ url }) {
 
 	if (hostname === 'localhost' || hostname === 'payto.money') {
 		// const hasValidProtocol = protocol === 'payto.money:';
-		const cleanPath = pathname.startsWith('/://') ? pathname.slice(3) : pathname;
-		const fullPath = cleanPath.startsWith('/') ? cleanPath.slice(1) : cleanPath;
-		const redirectUrl = `payto://${fullPath}${search}`;
+		const cleanPath = pathname.startsWith('/://') ? pathname.slice(3) : undefined;
+		const fullPath = cleanPath?.startsWith('/') ? cleanPath.slice(1) : cleanPath;
+		const redirectUrl = fullPath ? `/show?url=payto://${fullPath}${search}` : '';
 
 		if (fullPath) {
 			throw redirect(302, redirectUrl);
