25805c038d1a10efbacf988d9f65a4ab925e7bb3
Omid Nikrah <omidnikrah@gmail.com>
2025-02-19 13:36:48 +0330

    fix: fix showing the wallet card data in the show page

--- src/routes/show/+page.ts
+++ src/routes/show/+page.ts
@@ -1,6 +1,6 @@
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = ({ url }) => {
-	const urlParam = url.searchParams.get('url');
+	const urlParam = url.toString().split("?url=")[1];
 	return { urlParam };
 };
